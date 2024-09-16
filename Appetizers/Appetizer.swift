3bd93bbc1b873bbc231627b9b64dac35b12bfbb7
Mohamed-Mostafa7 <mohameds.mostafas7@gmail.com>
2024-09-16 23:29:14 +0300

    Add appetizer model

---
+++ Appetizers/Appetizer.swift
@@ -0,0 +1,36 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Mohamed Mostafa on 16/09/2024.
+//
+
+import Foundation
+
+struct Appetizer: Codable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 0001,
+                                           name: "Test Appetizer",
+                                           description: "This is the description for my appetizer. It's yummy",
+                                           price: 23.99,
+                                           imageURL: "",
+                                           calories: 45,
+                                           protein: 10,
+                                           carbs: 55)
+    
+    static let appetizers = [sampleAppetizer,sampleAppetizer,sampleAppetizer,sampleAppetizer,sampleAppetizer]
+}
