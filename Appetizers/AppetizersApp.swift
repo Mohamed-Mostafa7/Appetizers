9e577379819ff1582a544f7ef318be0c20ceb2eb
Mohamed-Mostafa7 <mohameds.mostafas7@gmail.com>
2024-09-16 18:09:59 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Mohamed Mostafa on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
