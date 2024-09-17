31348216bc877b08fd8cdc03eb685b3b9702acec
Mohamed-Mostafa7 <mohameds.mostafas7@gmail.com>
2024-09-17 23:24:45 +0300

    Add loading indicator to the appetizer list view

---
+++ Appetizers/ActivityIndicator.swift
@@ -0,0 +1,31 @@
+//
+//  ActivityIndicator.swift
+//  Appetizers
+//
+//  Created by Mohamed Mostafa on 17/09/2024.
+//
+
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        
+        let activityIndicatorView = UIActivityIndicatorView(style: .large)
+        activityIndicatorView.color = .brandPrimary
+        activityIndicatorView.startAnimating()
+        return activityIndicatorView
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) { }
+}
+
+struct loadingView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemBackground)
+                .ignoresSafeArea()
+            ActivityIndicator()
+        }
+    }
+}
---
+++ Appetizers/Color+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Extension.swift
+//  Appetizers
+//
+//  Created by Mohamed Mostafa on 17/09/2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimaryColor = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let brandPrimaryColor = UIColor(named:"brandPrimary")
+}
