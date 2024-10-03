d03a37e6e1eb5d9b19b0683bece48ac765baaaaa
Mohamed-Mostafa7 <mohameds.mostafas7@gmail.com>
2024-10-03 12:48:06 +0300

    Design Order view

--- Appetizers/Model/Appetizer.swift
+++ Appetizers/Model/Appetizer.swift
@@ -33,4 +33,32 @@ struct MockData {
                                            carbs: 55)
     
     static let appetizers = [sampleAppetizer,sampleAppetizer,sampleAppetizer,sampleAppetizer,sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+    
+    static let order1 = Appetizer(id: 0001,
+                                           name: "order 1",
+                                           description: "This is the description for my appetizer. It's yummy",
+                                           price: 23.99,
+                                           imageURL: "",
+                                           calories: 45,
+                                           protein: 10,
+                                           carbs: 55)
+    
+    static let order2 = Appetizer(id: 0002,
+                                           name: "order 2",
+                                           description: "This is the description for my appetizer. It's yummy",
+                                           price: 23.99,
+                                           imageURL: "",
+                                           calories: 45,
+                                           protein: 10,
+                                           carbs: 55)
+    
+    static let order3 = Appetizer(id: 0003,
+                                           name: "order 3",
+                                           description: "This is the description for my appetizer. It's yummy",
+                                           price: 23.99,
+                                           imageURL: "",
+                                           calories: 45,
+                                           protein: 10,
+                                           carbs: 55)
+    static let order = [order1, order2, order3]
 }
--- Appetizers/Screens/OrderView/OrderView.swift
+++ Appetizers/Screens/OrderView/OrderView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct OrderView: View {
+    @State var orderItems = MockData.order
+    
     var body: some View {
         NavigationView{
-            Text("Orders View")
-                .navigationTitle("Orders")
+            VStack{
+                
+                List {
+                    ForEach(orderItems) { appetizer in
+                        AppetizerListCell(appetizer: appetizer)
+                    }
+                    .onDelete(perform: deleteItem)
+                }
+                .listStyle(.plain)
+                
+                Button {
+                    print("Place Order Button Pressed")
+                } label: {
+                    APButton(title: "$30.99 - Place order")
+                }
+                .padding(25)
+
+            }
+            .navigationTitle("Order")
+            
         }
+    }
+    func deleteItem(at offset: IndexSet) {
+        orderItems.remove(atOffsets: offset)
     }
 }
 
